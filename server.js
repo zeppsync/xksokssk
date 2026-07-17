ac30e495f1380b3fc56baee5e92409f51a64ad94
zeppsync <zepysk@gmail.com>
2026-07-17 17:21:36 +0700

    N

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ function buildParams(phone) {
         simnum: '1',
         hasinrc: '1',
         pid: Math.floor(Math.random() * 1000).toString(),
-        id: Buffer.from(creds.identityId).toString('hex'),
+        id: Buffer.from(creds.identityId || crypto.randomBytes(16)).toString('hex'),
         backup_token: Buffer.from(creds.backupToken).toString('hex'),
         token: crypto.createHash('md5')
             .update(Buffer.concat([MOBILE_TOKEN, Buffer.from(national)]))
